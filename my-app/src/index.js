659f96321f5abce31939b5e4237ce5372f3841e8
yunsuuu <dbstnqls6953@naver.com>
2022-04-05 18:35:36 +0900

    .

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import App from './App';
 // import ReactDOM from 'react-dom';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
 // ReactDOM.render(
 //   <React.StrictMode>
---
+++ my-app/src/routes/Detail.js
@@ -0,0 +1,5 @@
+function Detail(){
+  return <h3>Detail</h3>
+}
+
+export default Detail;
