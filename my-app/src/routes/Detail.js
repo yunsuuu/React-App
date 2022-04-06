68a506db8d7b9af2ecd39d17fd738e18fbef93d0
yunsuuu <dbstnqls6953@naver.com>
2022-04-06 16:34:34 +0900

    .

--- my-app/src/routes/Detail.js
+++ my-app/src/routes/Detail.js
@@ -5,16 +5,28 @@ import { useParams } from "react-router-dom";
 
 function Detail(){
   const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [details, setDetails] = useState({});
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-  };
+    setDetails(json.data.movie);
+    setLoading(false);
+  }
   useEffect(() => {
     getMovie();
   }, []);
   return <div>
-    <h3>Loading...</h3>
+    <h2>Detail Page</h2>
+    { loading ?
+      <h3>Loading...</h3> :
+      <div>3
+        <img src={details.medium_cover_image}></img>
+        <h4>{details.title}</h4>
+        <p>{details.description_intro}</p>
+      </div>
+    }
   </div>
 }
 
